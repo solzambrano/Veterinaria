795c00ef141ab1a65eb55d77f71ba7622f68a668
Sol Zambrano <solzambrano.8@gmail.com>
2025-07-17 16:44:37 -0300

    se crea componente crecion de cuenta

---
+++ src/view/Login/CreateAcount.jsx
@@ -0,0 +1,36 @@
+import {useForm} from 'react-hook-form'
+
+const CreateAccount = () =>{
+    const {register,handleSubmit,formState:{errors}}=useForm()
+    const handleSubmitForm = (inputs) =>{
+        console.log(inputs);
+        
+    }
+    return(
+        <>
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+            <label for="name"> Nombre
+                <input type="text" {...register('Nombre',{reqired:true,maxLength:25})}
+                />
+            </label>
+             <label for="lastName"> Apellido
+                <input type="text" {...register('Apellido',{reqired:true,maxLength:25})}
+                />
+            </label>
+            <label for="phone"> Telefono
+                <input type="tel" {...register('Phone',{reqired:true})}/>
+            </label>
+            <label for ="email">Correo
+                <input type="email" {...register('Email',{reqired:true})}></input>
+            </label>
+            <label for="pass"> Contraseña
+                <input type="password" 
+              {...register('Password',{reqired:true,maxLength:8})}
+                />
+            </label>
+            <button type ="submit">Sign Up</button>
+        </form>
+        </>
+    )
+}
+export default CreateAccount
