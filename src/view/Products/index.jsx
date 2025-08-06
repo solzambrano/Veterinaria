679b491f238f02d068f6fda642cfd6ce1ba7bace
Sol Zambrano <solzambrano.8@gmail.com>
2025-08-06 18:54:40 -0300

    aplicacion de estilos

--- src/view/Products/index.jsx
+++ src/view/Products/index.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+import useFetchProducts from '../../hooks/useFetchProducts';
+import { ContainerImage } from './Products.style';
 
 const Products = () => {
+ const {data,getProducts}= useFetchProducts()
+const numberItems = data?.map((item)=>{
+   return <li key={item.id}>{item.name}</li>
+})
+  useEffect(() => {
+    getProducts();
+  }, []);
  return(
     <>
+    <h1>lista de productos </h1>
+    <ContainerImage>
+    <ul>{numberItems}</ul>
+    </ContainerImage>
     </>
  )   
 }
-export default Products
+export default Products  
