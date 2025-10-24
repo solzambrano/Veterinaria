9c07bf26e8ba98b69574ddf98cd974be0682a71d
Sol Zambrano <solzambrano.8@gmail.com>
2025-10-23 22:39:45 -0300

    linkeando redes sociales

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
         <ContainerFooter>
             <ContainerParagraph>
                 <Paragraph><NavLink to='politic'>Politica de Privacidad </NavLink></Paragraph>
-                <Paragraph><NavLink to='terms'> Térmios del Servicio</NavLink></Paragraph>
+                <Paragraph><NavLink to='terms'> Términos del Servicio</NavLink></Paragraph>
                 <Paragraph><NavLink to='contact'>Contacto</NavLink></Paragraph>
             </ContainerParagraph>
             <ContainerIcons>
-                <Icons src={Facebook} alt='icono facebook'></Icons>
-                <Icons src={Twitter} alt='icono twitter'></Icons>
-                <Icons src={Instagram} alt='icono instagram'></Icons>
+               <a href='https://www.facebook.com/'><Icons src={Facebook} alt='icono facebook'></Icons></a> 
+               <a href="https://x.com/?lang=es"> <Icons src={Twitter} alt='icono twitter'></Icons> </a>
+               <a href="https://www.instagram.com/">  <Icons src={Instagram} alt='icono instagram'></Icons></a>
             </ContainerIcons>
             <ContainerParagraph>
             <Reserved> @ Veterinaria Coquetos, todos los derechos reservados</Reserved>
