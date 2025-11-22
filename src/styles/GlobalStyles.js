860a23f8fbe8ad3735b8dd7c3ff0cb8209ddaae8
Sol Zambrano <solzambrano.8@gmail.com>
2025-11-22 00:00:07 -0300

    se añade fuente

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,6 +5,12 @@ export const GlobalStyles=createGlobalStyle`
 :root{
     --color-Green:#259b25;
     --color-Black:#00000;
+    --color-Ligth: #f6f8f6;
+    --color-Green-s: #37df37;
+    --font:'Lexend'
+}
+     body {
+    font-family: 'Lexend', sans-serif;
 }
 
 
