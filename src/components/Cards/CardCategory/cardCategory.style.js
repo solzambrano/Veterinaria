7706ab342b9ada6e95656d17929346587065ddd6
Sol Zambrano <solzambrano.8@gmail.com>
2025-12-24 20:32:37 -0300

    estilos a cajas categorys

--- src/components/Cards/CardCategory/cardCategory.style.js
+++ src/components/Cards/CardCategory/cardCategory.style.js
@@ -2,16 +2,28 @@ import styled from "styled-components";
 export const ContainerCategory =styled.div`
 display:flex;
 justify-content:center;
+cursor:pointer;
+:hover{
+box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+background-image:linear-gradient(to top, #46d841ff 0%, #18df82ff 100%)
+}
 `;
 
 export const Category =styled.div`
-border:1px solid black;
+border:1px solid #cef0be;
 margin:0px 15px;
 text-align:center;
 padding:15px 0px;
-border-radius:5px;
+border-radius:20px;
 width:250px;
+background-image: linear-gradient(to top, #9be15d 0%, #00e3ae 100%);
+
 `
 export const ImageCategory =styled.img`
-width:20px`
-export const TitleCategory = styled.h3``
+width:20px;
+
+`
+export const TitleCategory = styled.h3`
+pointer-events: none;
+
+`
