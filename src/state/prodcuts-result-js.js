f300e2d5d58fb114abaa68979bd96d5a4b589396
Sol Zambrano <57291427+solzambrano@users.noreply.github.com>
2026-06-01 23:24:28 -0300

    add paginator logic

--- src/state/prodcuts-result-js.js
+++ src/state/prodcuts-result-js.js
@@ -3,6 +3,8 @@ const productsResults = create ((set)=>({
     products:[],
     isLoading:true,
     error:{},
+    currentPage:1,
+    itemsPerPage:8,
 
     getProducts : async () =>{
          setTimeout(async () => {
@@ -17,6 +19,39 @@ const productsResults = create ((set)=>({
             set(()=>({error}))
         }
          }, 3000);
+    },
+    getCurrentItems: () => {
+        const { products, currentPage, itemsPerPage } = get()
+        const start = (currentPage - 1) * itemsPerPage
+        const end = start + itemsPerPage
+        return products.slice(start, end)
+},
+    getTotalPages:()=>{
+        const { products, itemsPerPage } = get()
+        return Math.ceil(products.length / itemsPerPage)
+    },
+    goToPage: (page) => {
+        const totalPages = get().getTotalPages()
+        if (page < 1 || page > totalPages) {
+            return    
+        }
+        set({ currentPage: page })
+    },
+
+    nextPage: () => {
+        const { currentPage, getTotalPages } = get()
+        if (currentPage < getTotalPages()) {
+            set({ currentPage: currentPage + 1 })
+        }
+    },
+    prevPage: () => {
+    const { currentPage } = get()
+    if (currentPage > 1) {
+        set({ currentPage: currentPage - 1 })
+    }
+  },
+    setPage :(page)=> {
+        set({currentPage:page})
     }
 
 }))
