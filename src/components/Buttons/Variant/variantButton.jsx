35aeb638e571137bbbdc923a8907a506b87206f7
Sol Zambrano <57291427+solzambrano@users.noreply.github.com>
2026-09-12 17:26:24 -0300

    se crea variantButton

---
+++ src/components/Buttons/Variant/variantButton.jsx
@@ -0,0 +1,24 @@
+export const buttonVariants = {
+  green: { bg: "var( --color-primary)", color: "var( --color-inverse)" },
+  pink: { bg: "var(--pink)", color: "var( --color-inverse)" },
+  pink_l: {
+    bg: "var(--color-danger-soft)",
+    color: "var(--color-danger-strong)",
+  },
+  brown: { bg: "var(--brown)", color: "var( --color-inverse)" },
+  red: { bg: "var(--color-danger)", color: "var( --color-inverse)" },
+};
+export const buttonSizes = {
+  small: {
+    padding: "6px 12px",
+    fontSize: "14px",
+  },
+  medium: {
+    padding: "10px 18px",
+    fontSize: "16px",
+  },
+  large: {
+    padding: "14px 24px",
+    fontSize: "18px",
+  },
+};
