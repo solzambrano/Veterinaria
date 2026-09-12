c97538e66988d1d4fb3e8499a7e1d69af5dd1f44
Sol Zambrano <57291427+solzambrano@users.noreply.github.com>
2026-09-12 17:27:51 -0300

    correccion color category

--- src/view/Help/CardCategory/cardCategory.style.js
+++ src/view/Help/CardCategory/cardCategory.style.js
@@ -1,29 +1,26 @@
 import styled from "styled-components";
-export const ContainerCategory =styled.div`
-display:flex;
-justify-content:center;
-cursor:pointer;
-
+export const ContainerCategory = styled.div`
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
 `;
 
-export const Category =styled.div`
-border:1px solid #cef0be;
-margin:0px 15px;
-text-align:center;
-padding:15px 0px;
-border-radius:20px;
-width:250px;
-background-image: linear-gradient(to top, #bfe4b8 0%, #b7e4d9 100%);
-&:hover{
-box-shadow: 0 4px 8px rgba(82, 230, 173, 0.2);
-background-image:linear-gradient(to top, #46d841ff 0%, #18df82ff 100%)
-}
-`
-export const ImageCategory =styled.img`
-width:20px;
-
-`
+export const Category = styled.div`
+  border: 1px solid var(--green-50-solid);
+  margin: 0px 15px;
+  text-align: center;
+  padding: 15px 0px;
+  border-radius: 20px;
+  width: 250px;
+  background-image: var(--gradient-green-soft);
+  &:hover {
+    box-shadow: 0 4px 8px var(--teal-soft);
+    background-image: var(--gradient-green-soft);
+  }
+`;
+export const ImageCategory = styled.img`
+  width: 20px;
+`;
 export const TitleCategory = styled.h3`
-pointer-events: none;
-
-`
+  pointer-events: none;
+`;
--- src/view/Help/CardCategory/index.jsx
+++ src/view/Help/CardCategory/index.jsx
@@ -1,17 +1,25 @@
 import Categories from "../../../../public/data/data-category";
-import {ContainerCategory,Category,ImageCategory,TitleCategory}from './cardCategory.style'
-              
-const CardCategory = ({onSelectedCategory}) => {
+import {
+  ContainerCategory,
+  Category,
+  ImageCategory,
+  TitleCategory,
+} from "./cardCategory.style";
 
-return(
+const CardCategory = ({ onSelectedCategory }) => {
+  return (
     <ContainerCategory>
-{Categories.map(category => {
-   return  <Category  key={category.id}
-             onClick={() => onSelectedCategory(category.slug)}>
-                <ImageCategory src={category.image}></ImageCategory>
-                <TitleCategory>{category.title}</TitleCategory>
-            </Category>
-})}
+      {Categories.map((category) => {
+        return (
+          <Category
+            key={category.id}
+            onClick={() => onSelectedCategory(category.slug)}>
+            <ImageCategory src={category.image}></ImageCategory>
+            <TitleCategory>{category.title}</TitleCategory>
+          </Category>
+        );
+      })}
     </ContainerCategory>
-)
-};export default CardCategory
+  );
+};
+export default CardCategory;
